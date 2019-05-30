f0e37c6c1168a119c5c737b9d4b6cce68bc0d378
ironypk <ironypk@gmail.com>
2019-05-30 13:48:27 +1200

    js on: fixed-menu, team , menu;

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-console.log ("Привет");
-
-
 //////////////FIXED- NAVBAR
 
 
@@ -45,8 +42,9 @@ for(let i = 0; i < fixedNavbarItemLength; i++){
 }
 
 //////////////////////////TEAM 
-
-    teamItemName = document.querySelectorAll('.name'),
+    let teamItem = document.querySelectorAll('.team__item'),
+    teamItemLength = teamItem.length;
+    let teamItemName = document.querySelectorAll('.name'),
     teamItemNameLength = teamItemName.length;
 
 
@@ -55,6 +53,9 @@ for( let i=0; i<teamItemNameLength; i ++){
         if (teamItemName[i].parentNode.classList.contains('team__item--active')){
                         teamItemName[i].parentNode.classList.remove('team__item--active')
                     } else{
+                        for( let i=0; i<teamItemLength; i ++){
+                            teamItem[i].classList.remove('team__item--active');
+                        }
                         teamItemName[i].parentNode.classList.add('team__item--active')
                     } 
     });
